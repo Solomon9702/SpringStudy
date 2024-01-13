b2ff1df9444d12581c13517ad8c59b49ac2e768f
chanho <thdcksgh15@gmail.com>
2024-01-13 21:48:28 +0900

    view

--- src/main/java/org/example/SpringBootDeveloperApplication.java
+++ src/main/java/org/example/SpringBootDeveloperApplication.java
@@ -1,8 +1,10 @@
-package org.example.springbootdeveloper;
+package org.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing  // created_at, updated_at 자동 업데이트
 @SpringBootApplication
 public class SpringBootDeveloperApplication {
     public static void main(String[] args) {
