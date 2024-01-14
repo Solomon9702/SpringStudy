41582daec5990ced81d4a519df0bee791557ff30
chanho <thdcksgh15@gmail.com>
2024-01-14 11:34:50 +0900

    before pdf

--- src/main/java/org/example/springbootdeveloper/controller/BlogViewController.java
+++ src/main/java/org/example/springbootdeveloper/controller/BlogViewController.java
@@ -1,11 +1,15 @@
 package org.example.springbootdeveloper.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.example.springbootdeveloper.domain.Article;
 import org.example.springbootdeveloper.dto.ArticleListViewResponse;
+import org.example.springbootdeveloper.dto.ArticleViewResponse;
 import org.example.springbootdeveloper.service.BlogService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -23,5 +27,26 @@ public class BlogViewController {
         model.addAttribute("articles", articles);  // 블로그 글 리스트 저장
 
         return "articleList";
+    }
+
+    @GetMapping("/articles/{id}")
+    public String getArticle(@PathVariable Long id, Model model){
+        Article article = blogService.findById(id);
+        model.addAttribute("article", new ArticleViewResponse(article));
+
+        return "article";
+    }
+
+    @GetMapping("/new-article")
+    public String newArticle(@RequestParam(required = false) Long id, Model model)
+    {
+        if (id == null){
+            model.addAttribute("article", new ArticleViewResponse());
+        } else{
+            Article article = blogService.findById(id);
+            model.addAttribute("article", new ArticleViewResponse(article));
+        }
+
+        return "newArticle";
     }
 }
